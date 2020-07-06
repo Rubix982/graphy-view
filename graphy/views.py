1123760f8f032347429d5c126586c7a299480d38
Saif Ul Islam <saifulislam84210@gmail.com>
2020-07-06 14:08:32 +0500

    Add basic boiler plate code

---
+++ graphy/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+from bokeh.plotting import figure, output_file, show
+from bokeh.embed import components
+
+# Create your views here.
+def index(request):
+
+    x = [ 1, 2, 3, 4, 5 ]
+    y = [ 1, 2, 3, 4, 5 ]
+
+    plot = figure(title = "Line Graph", x_axis_label="X-Axis", y_axis_label="Y-Axis", plot_width=400, plot_height=400)
+
+    plot.line(x, y, line_width=2)
+
+    script, div = components(plot)
+
+    return render(request, "graphy/base.html", {'script': script, 'div': div} )
---
+++ graphyView/urls.py
@@ -0,0 +1,24 @@
+"""graphyView URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+"""
+# Django imports
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    # Examples:
+    # url(r'^blog/', include('blog.urls', namespace='blog')),
+
+    # provide the most basic login/logout functionality
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='core/login.html'),
+        name='core_login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='core_logout'),
+
+    # enable the admin interface
+    url(r'^admin/', admin.site.urls),
+
+    url('', include('graphy.urls') ),
+]
